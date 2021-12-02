da745518fed89ef7f016b7c1ab697f4835681d8e
Maryana <ilon.mars@yandex.ru>
2021-12-02 21:24:14 +0300

    update: title at browser tab 🧰

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/zanachka/'
-    : ''
+    : '',
+
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'ZANACHKA - калькулятор бюджета'
+        return args
+      })
+  }
 }
