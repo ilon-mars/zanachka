817479f2e8971afc5e09739b3c75b1e46e74fccc
Maryana <ilon.mars@yandex.ru>
2021-12-09 21:38:49 +0300

    update: componet for jars method 🌵

--- src/views/JarsBudget.vue
+++ src/views/JarsBudget.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    jars
-  </div>
+  <fieldset class="calculate__budget-parameters">
+    <div class="calculate__budget-parameter">
+      <label for="">На жизнь (55%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На развлечения (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На сбережения (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На образование (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На черный день (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На подарки (5%)</label>
+      <input type="text" id="" readonly>
+    </div>
+  </fieldset>
 </template>
