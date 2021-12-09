4940d83da892c9db1e5136740d582cecf29224db
Maryana <ilon.mars@yandex.ru>
2021-12-09 21:40:22 +0300

    update: component for microsoft method

--- src/views/MicrosoftBudget.vue
+++ src/views/MicrosoftBudget.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    microsoft
-  </div>
+  <fieldset class="calculate__budget-parameters">
+    <div class="calculate__budget-parameter">
+      <label for="">На жизнь (60%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На инвестиции, пенсионные накопления (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На сбережения (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На резерв (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На развлечения, включая праздники (10%)</label>
+      <input type="text" id="" readonly>
+    </div>
+  </fieldset>
 </template>
