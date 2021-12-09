17b3911a13fddda7077a62bc0f46481f5378af89
Maryana <ilon.mars@yandex.ru>
2021-12-09 21:46:22 +0300

    update: componet for envelopes method

--- src/views/EnvelopesBudget.vue
+++ src/views/EnvelopesBudget.vue
@@ -1,3 +1,47 @@
 <template>
-  <div>envelopes </div>
+  <fieldset class="calculate__budget-parameters">
+    <div class="calculate__budget-parameter">
+      <label for="">Питание</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">Одежда</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">Лекарства</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">Машина</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">Коммунальные услуги</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">Развлечения</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">Накопления</label>
+      <input type="text" id="">
+      <button type="button">Удалить этот конверт</button>
+    </div>
+
+    <button type="button">Добавить еще конверт</button>
+  </fieldset>
 </template>
