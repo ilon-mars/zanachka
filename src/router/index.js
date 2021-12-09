66d083726c1a1b423c9457ddb6fa09d984ae59d0
Maryana <ilon.mars@yandex.ru>
2021-12-09 20:19:07 +0300

    add: router 🌵

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/envelopes',
+    name: 'CalculateBudget',
+    component: () => import(/* webpackChunkName: "envelopes" */ '../views/EnvelopesBudget.vue')
+  },
+  {
+    path: '/jars',
+    name: 'CalculateBudget',
+    component: () => import(/* webpackChunkName: "jars" */ '../views/JarsBudget.vue')
+  },
+  {
+    path: '/microsoft',
+    name: 'CalculateBudget',
+    component: () => import(/* webpackChunkName: "microsoft" */ '../views/MicrosoftBudget.vue')
+  },
+  {
+    path: '/4-envelopes',
+    name: 'CalculateBudget',
+    component: () => import(/* webpackChunkName: "4-envelopes" */ '../views/4EnvelopesBudget.vue')
+  },
+  {
+    path: '/50-30-20',
+    name: 'CalculateBudget',
+    component: () => import(/* webpackChunkName: "50-30-20" */ '../views/503020Budget.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
