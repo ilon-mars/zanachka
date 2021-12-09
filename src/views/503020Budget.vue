ff703c1564ea0c3a59ec9bfe059675dad1b8140b
Maryana <ilon.mars@yandex.ru>
2021-12-09 21:35:45 +0300

    update: component for 50-30-20 method 🌵

--- src/views/503020Budget.vue
+++ src/views/503020Budget.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    50-30-20
-  </div>
+  <fieldset class="calculate__budget-parameters">
+    <div class="calculate__budget-parameter">
+      <label for="">На повседневные нужды (50%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На себя (30%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+    <div class="calculate__budget-parameter">
+      <label for="">На черный день, инвестиции или на погашение долгов (20%)</label>
+      <input type="text" id="" readonly>
+    </div>
+
+  </fieldset>
 </template>
