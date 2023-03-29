f963643a30f603f5ff5774f969915b0a41d36f6f
Maryana <ilon.mars@yandex.ru>
2023-03-29 17:40:04 +0300

    add: styles ✨

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import '@/assets/styles/main.sass'
+
 const app = createApp(App)
 
 app.use(createPinia())
