e8254fa84c305d85eb830072dac79891eaa67f29
Maryana <ilon.mars@yandex.ru>
2023-04-08 21:54:04 +0300

    add: easy method 🌵

---
+++ src/stores/easy.ts
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { generateId, wearMask } from '@/utils/functions';
+import { FixedEnvelopesModel } from '@/models/Method';
+import { getFromLS, saveToLS, removeFromLS } from '@/services';
+import { MethodEnum } from '@/enums';
+import { EASY_PERCENTS } from '@/utils/constants';
+
+export const useEasyStore = defineStore('easy', () => {
+  /* init localStorage */
+  const localData: FixedEnvelopesModel = getFromLS(MethodEnum.EASY);
+
+  /* component logic */
+  const income = ref<string>(localData?.income.toString() || '');
+  const fieldsList = computed(() =>
+    EASY_PERCENTS.map((percent, index) => ({
+      id: generateId(`field-${index}-`),
+      name: `${percent}%`,
+      amount: wearMask(Math.floor((+income.value / 100) * percent)),
+    }))
+  );
+
+  const save = () => {
+    if (!income.value) return;
+
+    const budget = new FixedEnvelopesModel(income.value);
+
+    saveToLS(MethodEnum.EASY, budget);
+  };
+
+  const clear = () => {
+    income.value = '';
+
+    removeFromLS(MethodEnum.EASY);
+  };
+
+  return {
+    income,
+    fieldsList,
+    save,
+    clear,
+  };
+});
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,4 @@
 export * from './classicEnvelopes';
 export * from './jars';
 export * from './microsoft';
+export * from './easy';
