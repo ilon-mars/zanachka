7353a7e02c66ee81c81f42b1f2938523e97dc9fc
Maryana <ilon.mars@yandex.ru>
2023-04-09 22:00:17 +0300

    update: configs ⚙️

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 const options = {
   plugins: [vue()],
@@ -20,12 +20,12 @@ const options = {
       },
     },
   },
-}
+};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
     base: command === 'build' ? '/zanachka/' : '',
     ...options,
-  }
-})
+  };
+});
