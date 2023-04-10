fb1213efe9e2556a4d1855ca5ac418e7a502fde9
Maryana <ilon.mars@yandex.ru>
2023-04-10 12:59:28 +0300

    add: footer 🌵

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
     <MethodsSection />
     <CalculatorSection />
   </main>
+  <AppFooter />
 </template>
 
 <script setup lang="ts">
 import CoverSection from '@/components/CoverSection.vue';
 import MethodsSection from '@/components/MethodsSection.vue';
 import CalculatorSection from '@/components/CalculatorSection.vue';
+import AppFooter from '@/components/AppFooter.vue';
 </script>
